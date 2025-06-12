376e384fd976b6a3fb77a1639f5aa6c245643612
Jan Vogt <jan@noreply.vogts.cloud>
2025-06-13 01:01:10 +0200

    Add more tools to dev env
    Also downgrade to ghc910, as ghc912 didn't
    work well with haskell-language-server

--- flake.nix
+++ flake.nix
@@ -7,12 +7,16 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        hpgkgs = pkgs.haskell.packages.ghc912;
+        hpgkgs = pkgs.haskell.packages.ghc910;
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             hpgkgs.ghc
             hpgkgs.haskell-language-server
+            hpgkgs.cabal-gild
+            hpgkgs.doctest
+            # The following is necessary for hls, since it appears not to respect build-tool-depends
+            hpgkgs.tasty-autocollect
             nodejs
             cabal-install
             ghcid
