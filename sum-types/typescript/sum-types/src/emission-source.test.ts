abaaa80350f57fee1602fb4397ca77f1a4b33a4f
Thomas Liebetraut <thomas.liebetraut@enit-systems.com>
2025-06-14 14:15:33 +0200

    first complete version, including tests, of typescript 'bad' example

---
+++ sum-types/typescript/sum-types/src/emission-source.test.ts
@@ -0,0 +1,77 @@
+import {
+  EMISSION_FACTOR_TABLE,
+  getInitialFormValues,
+  makeEmitter,
+} from "./emission-source";
+
+describe("getInitialFormValues() sets all the form default values", () => {
+  test("a simple emitter with emission factor taken from database", () => {
+    const simpleEmitter = makeEmitter({
+      emitterId: "ELECTRICITY_GERMANY",
+      description: "Some Utility Company",
+      quantity: { value: 9001, uncertainty: 10 },
+    });
+
+    expect(getInitialFormValues(simpleEmitter, EMISSION_FACTOR_TABLE)).toEqual({
+      description: "Some Utility Company",
+      emitterId: "ELECTRICITY_GERMANY",
+      quantity: "9001",
+      quantityUncertainty: 10,
+
+      // values computed from input
+      factorMode: "database",
+      factor: "0.4",
+      factorUncertainty: 30,
+      emissions: "3.6004",
+      emissionsUncertainty: 30,
+
+      enterEmissionsManually: false,
+    });
+  });
+
+  test("a emitter with completely manually entered emissions", () => {
+    const manualEmitter = makeEmitter({
+      description: "Manually estimated GHG emissions",
+      emissions: { value: 50_000, uncertainty: 20 },
+    });
+
+    expect(getInitialFormValues(manualEmitter, EMISSION_FACTOR_TABLE)).toEqual({
+      description: "Manually estimated GHG emissions",
+      enterEmissionsManually: true,
+      emissions: "50000",
+      emissionsUncertainty: 20,
+
+      // values "computed" from input
+      emitterId: undefined,
+      factorMode: undefined,
+      factor: undefined,
+      factorUncertainty: 30,
+      quantity: undefined,
+      quantityUncertainty: 30,
+    });
+  });
+
+  test("a emitter with known quantity and emission factor not from database", () => {
+    const manualEmitter = makeEmitter({
+      description: "Product from supplier",
+      quantity: { value: 100, uncertainty: 0 },
+      factor: { value: 0.5, uncertainty: 5 },
+    });
+
+    expect(getInitialFormValues(manualEmitter, EMISSION_FACTOR_TABLE)).toEqual({
+      description: "Product from supplier",
+      quantity: "100",
+      quantityUncertainty: 0,
+      factorMode: "manual",
+      factor: "0.5",
+      factorUncertainty: 5,
+
+      // values computed from input
+      emissions: "0.05",
+      emissionsUncertainty: 30,
+
+      enterEmissionsManually: false,
+      emitterId: undefined,
+    });
+  });
+});
--- sum-types/typescript/sum-types/src/emission-source.ts
+++ sum-types/typescript/sum-types/src/emission-source.ts
@@ -1,70 +1,150 @@
-export type ValueWithUncertainty = Readonly<{ value: number; uncertainty: number }>;
+export type ValueWithUncertainty = Readonly<{
+  value: number;
+  uncertainty: number;
+}>;
+
+export const EMISSION_FACTOR_TABLE = {
+  ELECTRICITY_GERMANY: 0.4,
+  NATURAL_GAS: 0.2,
+} as const;
+
+type EmitterFactors = typeof EMISSION_FACTOR_TABLE;
+
+function getFactorFromDatabase(
+  factors: EmitterFactors,
+  emitterId: string,
+): number | undefined {
+  return factors[emitterId as keyof EmitterFactors];
+}
+
+export function calculateEmissionsInTons(
+  quantity?: number,
+  factor?: number,
+  factorsFromDatabase?: EmitterFactors,
+  emitterId?: string,
+): number {
+  if (quantity && factor) {
+    return (quantity * factor) / 1000;
+  } else if (
+    quantity &&
+    factorsFromDatabase &&
+    emitterId &&
+    getFactorFromDatabase(factorsFromDatabase, emitterId)
+  ) {
+    return (
+      (quantity * getFactorFromDatabase(factorsFromDatabase, emitterId)!) / 1000
+    );
+  } else {
+    return 0;
+  }
+}
 
 export type Emitter = {
-    description: string;
-    getEmissions: (emitterFactors: any) => number;
-    getUncertaintyOfEmissions: () => number;
-} & EmitterOptions;
-
-export type EmitterOptions = {
-    assignee?: string;
-    quantity?: ValueWithUncertainty;
-    factor?: ValueWithUncertainty;
-    emissions?: ValueWithUncertainty;
+  description: string;
+  emitterId?: string;
+  quantity?: ValueWithUncertainty;
+  factor?: ValueWithUncertainty;
+  emissions?: ValueWithUncertainty;
+  getEmissions: (emitterFactors: EmitterFactors) => number;
+  getUncertaintyOfEmissions: () => number;
 };
 
+export function makeEmitter(
+  options: Omit<Emitter, "getEmissions" | "getUncertaintyOfEmissions">,
+): Emitter {
+  function getFactor(emitterFactors: EmitterFactors): number | undefined {
+    if (Number.isFinite(options?.factor?.value)) {
+      return options?.factor?.value;
+    } else if (options?.emitterId) {
+      return emitterFactors[options?.emitterId]?.factor;
+    } else {
+      return undefined;
+    }
+  }
+
+  return {
+    ...options,
+    getEmissions(emitterFactors) {
+      if (options?.emissions != null) {
+        return options.emissions.value;
+      }
+      if (options?.quantity == null) {
+        return 0;
+      }
+
+      const factor = getFactor(emitterFactors);
+      if (factor == null) {
+        return 0;
+      }
+
+      return (factor * options.quantity.value) / 1000;
+    },
+    getUncertaintyOfEmissions() {
+      if (options?.emissions != null) {
+        return options.emissions.uncertainty;
+      }
+
+      if (options?.quantity != null && options?.factor != null) {
+        return Math.sqrt(
+          Math.pow(Number(options?.quantity?.uncertainty), 2) +
+            Math.pow(Number(options?.factor?.uncertainty), 2),
+        );
+      }
+
+      return 60;
+    },
+  };
+}
 
 export type EmitterFormValues = {
-    description: string;
-    assignee?: string;
-    category: string;
-    quantity?: string;
-    quantityUncertainty?: number;
-    factor?: string;
-    factorUncertainty?: number;
-    factorMode: "manual" | "database";
-    enterEmissionsManually: boolean;
-    emissions?: string;
-    emissionsUncertainty?: number;
+  description: string;
+  quantity?: string;
+  quantityUncertainty?: number;
+  emitterId?: keyof EmitterFactors;
+  factor?: string;
+  factorUncertainty?: number;
+  factorMode: "manual" | "database";
+  enterEmissionsManually: boolean;
+  emissions?: string;
+  emissionsUncertainty?: number;
 };
 
-
-
 export function getInitialFormValues(
-    emitter: Emitter,
-    factors: EmitterFactors
+  emitter: Emitter,
+  factors: EmitterFactors,
 ): Partial<EmitterFormValues> {
-    let valuesFromEmitter: Partial<EmitterFormValues> = {};
 
+  let valuesFromEmitter: Partial<EmitterFormValues> = {
+    description: emitter.description,
+    emitterId: Object.keys(factors).includes(emitter.emitterId) ? emitter.emitterId as keyof EmitterFactors : undefined,
+    quantity: emitter.quantity?.value.toString(),
+    quantityUncertainty: emitter.quantity?.uncertainty ?? 30,
+    emissionsUncertainty: emitter.emissions?.uncertainty ?? 30,
+    factorUncertainty: emitter.factor?.uncertainty ?? 30,
+  };
+  if (emitter.emissions != null) {
+    // manual mode, emissions are entered directly
     valuesFromEmitter = {
-        description: emitter.description,
-        assignee: emitter.assignee,
-        quantity: emitter.quantity?.value.toString(),
-        quantityUncertainty: emitter.quantity?.uncertainty ?? 30,
-        emissionsUncertainty: emitter.emissions?.uncertainty ?? 30,
-        factorUncertainty: emitter.factor?.uncertainty ?? 30,
+      ...valuesFromEmitter,
+      enterEmissionsManually: true,
+      emissions: emitter.emissions?.value.toString(),
     };
-    if (emitter.emissions != null) {
-        // manual mode, emissions are entered directly
-        valuesFromEmitter = {
-            ...valuesFromEmitter,
-            enterEmissionsManually: true,
-            emissions: emitter.emissions?.value.toString(),
-        };
-    } else {
-        valuesFromEmitter = {
-            ...valuesFromEmitter,
-            factorMode: emitter.factor == null ? "database" : "manual",
-            factor: emitter.factor?.value.toString() ?? getFactorFromDatabase(factors, emitter.emitterId),
-            enterEmissionsManually: false,
-            emissions: calculateEmissionsInTons(
-                emitter.quantity?.value,
-                emitter.factor?.value,
-                factors,
-                emitter.emitterId
-            ).toString(),
-        };
-    }
+  } else {
+    valuesFromEmitter = {
+      ...valuesFromEmitter,
+      factorMode: emitter.factor == null ? "database" : "manual",
+      factor:
+        emitter.factor?.value.toString() ??
+        getFactorFromDatabase(factors, emitter.emitterId)?.toString(),
+      enterEmissionsManually: false,
+      emissions: calculateEmissionsInTons(
+        emitter.quantity?.value,
+        emitter.factor?.value,
+        factors,
+        emitter.emitterId,
+      ).toString(),
+    };
+  }
 
-    return valuesFromEmitter;
+  return valuesFromEmitter;
 }
